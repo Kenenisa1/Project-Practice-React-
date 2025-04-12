6551c500fe2858ddfbaf157626b4434f5466a724
Kenenisa <kenenisamiesob.com>
2025-04-13 01:30:06 +0300

    FoodList component is done with html and css

--- ReactMini/src/FoodList.jsx
+++ ReactMini/src/FoodList.jsx
@@ -1,28 +1,32 @@
 import React, {useState} from 'react'
 
 const FoodList = () => {
-    const [food, setFd] = (["Apple", "Honey", "Mushabek"]);
+    const [food, setFd] = useState(["Apple", "Honey", "Mushabek"]);
 
     function handleAdFood(){
-        const newFood= document.getElementById("foodInput");
-        
+        const newFood= document.getElementById("foodInput").value;
+        document.getElementById("foodInput").value="";
+        setFd( f=> [...f,newFood]);
+
     }
 
-    function handleRemoveFood(){
-
+    function handleRemoveFood(index){
+        setFd(food.filter((_, i) => i!== index));
     }
 
 
   return (
     <div>
-        <h2>FoodList</h2>
+        <h1>Welcome to Halal Restorant</h1>
+        <h2>Choose your delicous</h2>
         <ul>
             {
                 food.map((fd,index) => 
-                <li key={index}>{fd}</li> )
+                <li key={index} onClick={ () => handleRemoveFood(index)} >{fd}</li> )
             }
         </ul>
         <input type="text" id='foodInput' placeholder='enter food name' />
+        <button onClick={handleAdFood}>Add food</button>
 
     </div>
   )
